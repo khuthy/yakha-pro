7c673def565e9825dbb5bfe4142a0ac73a092729
khuthy <muendakhuthadzo@gmail.com>
2019-10-26 14:55:16 +0200

    puah

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,17 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'View Profile', component: HomePage, icon: 'ios-person' },
+      { title: 'Tips', component: HomePage, icon: 'information-circle' },
+      { title: 'Version', component: HomePage, icon: 'help' }
+
     ];
 
   }
