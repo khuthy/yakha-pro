30130a8effa47b2ad25208c31c089905a4f24278
Muenda Khuthadzo <muendakhuthadzo@gmail.com>
2019-10-28 08:38:14 -0700

    pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { Keyboard } from '@ionic-native/keyboard';
 import { HttpClient } from '@angular/common/http';
 // import { PlacePage } from './../pages/place/place';
 //import { AddBricklayerPage } from './../pages/add-bricklayer/add-bricklayer';
-
-// import { BaccountSetupPage } from './../pages/baccount-setup/baccount-setup';
+ import { BaccountSetupPage } from './../pages/baccount-setup/baccount-setup';
 // import { Downloader } from '@ionic-native/downloader';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { BuilderProfileviewPage } from './../pages/builder-profileview/builder-profileview';
@@ -14,7 +13,7 @@ import { BuilderProfileviewPage } from './../pages/builder-profileview/builder-p
 // import { VersionPage } from './../pages/version/version';
 
 // import { FeedbackPage } from './../pages/feedback/feedback';
-// import { HelpPage } from './../pages/help/help';
+ import { HelpPage } from './../pages/help/help';
 // import { MessagesPage } from './../pages/messages/messages';
 
 // import { AccountSetupPage} from './../pages/account-setup/account-setup';
@@ -56,7 +55,7 @@ import { IonicStorageModule } from '@ionic/storage';
 
 //import { BuilderquotesPage } from '../pages/builderquotes/builderquotes';
 
-//import { File } from '@ionic-native/file';
+import { File } from '@ionic-native/file';
 
 //import { FileOpener } from '@ionic-native/file-opener';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -73,10 +72,12 @@ import { RegisterPage } from '../pages/register/register';
 import { WelcomePage } from '../pages/welcome/welcome';
 import { firebaseConfig } from './app.firebase.config';
 import * as firebase from 'firebase';
+import { VersionPage } from '../pages/version/version';
 //import { OneSignal } from '@ionic-native/onesignal';
 //import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 //import { SMS } from '@ionic-native/sms';
-//import { ChannelsPage } from '../pages/channels/channels';
+import { ChannelsPage } from '../pages/channels/channels';
+import { TipsPage } from '../pages/tips/tips';
 //import { PasswordResetComponent } from '../components/password-reset/password-reset';
 firebase.initializeApp(firebaseConfig);
 @NgModule({
@@ -92,7 +93,12 @@ firebase.initializeApp(firebaseConfig);
     RegisterPage,
     OnboardingPage,
     OnboardingBuilderPage,
-    SuccessPage
+    SuccessPage,
+    BaccountSetupPage,
+    HelpPage,
+    ChannelsPage,
+    VersionPage,
+    TipsPage
 
   
   ],
@@ -114,7 +120,12 @@ firebase.initializeApp(firebaseConfig);
     RegisterPage,
     OnboardingPage,
     OnboardingBuilderPage,
-    SuccessPage
+    SuccessPage,
+    BaccountSetupPage,
+    HelpPage,
+    VersionPage,
+    ChannelsPage,
+    TipsPage
     
   ],
   providers: [
